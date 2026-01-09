5d5c585d5bf17bef1cf07931218c2ba7fd8d0195
Borislav-Madzharov <borislavmadzharov10@gmail.com>
2026-01-09 17:31:15 +0200

    Add files via upload

---
+++ Programming-Fundamentals-with-C#-Sep2025/06.ObjectsAndClasses-Exercise/07.VehicleCatalogue/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+car Lamborghini green 612
+car Mercedes-Benz gray 204
+truck Man red 200
+truck Daff white 212
+End
+Lamborghini
+Man
+Mercedes-Benz
+Close the Catalogue
+
+
+ 
+ 
+ */
+namespace _06.VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string command = "";
+            int averageHPCar = 0;
+            int carCount = 0;
+            int averageHPTruck = 0;
+            int truckCount = 0;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] arguments = command.Split(' ');
+
+                if (arguments[0] == "car")
+                {
+                    Vehicle vehicle = new Vehicle();
+                    vehicle.Type = "Car";
+                    vehicle.Model = arguments[1];
+                    vehicle.Color = arguments[2];
+                    vehicle.Horsepower = int.Parse(arguments[3]);
+                    vehicles.Add(vehicle);
+
+
+                    averageHPCar += int.Parse(arguments[3]);
+                    carCount++;
+                }
+                else if (arguments[0] == "truck")
+                {
+                    Vehicle vehicle = new Vehicle();
+                    vehicle.Type = "Truck";
+                    vehicle.Model = arguments[1];
+                    vehicle.Color = arguments[2];
+                    vehicle.Horsepower = int.Parse(arguments[3]);
+                    vehicles.Add(vehicle);
+
+
+                    averageHPTruck += int.Parse(arguments[3]);
+                    truckCount++;
+                }
+            }
+
+            string otherCommand = "";
+            while ((otherCommand = Console.ReadLine()) != "Close the Catalogue")
+            {
+                foreach (Vehicle vehicle in vehicles)
+                {
+                    if (otherCommand == vehicle.Model)
+                    {
+                        Console.WriteLine($"Type: {vehicle.Type}");
+                        Console.WriteLine($"Model: {vehicle.Model}");
+                        Console.WriteLine($"Color: {vehicle.Color}");
+                        Console.WriteLine($"Horsepower: {vehicle.Horsepower}");
+                    }
+
+
+                }
+            }
+
+            if (carCount > 0)
+            {
+                Console.WriteLine($"Cars have average horsepower of: {((decimal)averageHPCar / carCount):f2}.");
+            }
+            if (truckCount > 0)
+            {
+                Console.WriteLine($"Trucks have average horsepower of: {((decimal)averageHPTruck / truckCount):f2}.");
+            }
+
+
+
+
+
+
+        }
+    }
+
+
+    public class Vehicle
+    {
+        public string Type;
+        public string Model;
+        public string Color;
+        public int Horsepower;
+    }
+}
---
+++ Programming-Fundamentals-with-C#-Sep2025/06.ObjectsAndClasses-Exercise/08.OrderByAge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+George 1024 12
+George 1024 13
+End
+ 
+ */
+namespace _07.OrderByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonIdentification> People = new List<PersonIdentification>();
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] data = command.Split(' ');
+                string firstName = data[0];
+                string ID = data[1];
+                string age = data[2];
+                if (People.Find(p => p.Id == ID) != null)
+                {
+                    int index = People.FindIndex((p => p.Id == ID));
+
+                    People[index].FirstName = firstName;
+                    People[index].Age = int.Parse(age);
+                }
+                else
+                {
+                    PersonIdentification person = new PersonIdentification();
+                    person.FirstName = firstName;
+                    person.Age = int.Parse(age);
+                    person.Id = ID;
+                    People.Add(person);
+
+                }
+
+
+
+            }
+
+            var sorted = People.OrderBy(x => x.Age).ToList();
+
+
+            foreach (PersonIdentification person in sorted)
+            {
+                Console.WriteLine($"{person.FirstName} with ID: {person.Id} is {person.Age} years old.");
+            }
+
+
+
+
+
+
+        }
+    }
+    public class PersonIdentification
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
