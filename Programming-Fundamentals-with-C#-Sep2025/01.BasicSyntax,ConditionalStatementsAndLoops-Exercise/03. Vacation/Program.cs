71b01ae27c813cae9a7a7a5b23050a18deb102a4
Borislav-Madzharov <borislavmadzharov10@gmail.com>
2026-01-09 16:06:33 +0200

    Add files via upload

---
+++ Programming-Fundamentals-with-C#-Sep2025/01.BasicSyntax,ConditionalStatementsAndLoops-Exercise/03. Vacation/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            double price = 0;
+            double totalPrice = 0;
+            double Discount = 1;
+
+            if (day == "Friday")
+            {
+                if (type == "Students")
+                {
+                    price = 8.45;
+                }
+                else if (type == "Business")
+                {
+                    price = 10.90;
+                }
+                else if (type == "Regular")
+                {
+                    price = 15;
+                }
+            }
+            else if (day == "Saturday")
+            {
+                if (type == "Students")
+                {
+                    price = 9.80;
+                }
+                else if (type == "Business")
+                {
+                    price = 15.60;
+                }
+                else if (type == "Regular")
+                {
+                    price = 20;
+                }
+            }
+            else if (day == "Sunday")
+            {
+                if (type == "Students")
+                {
+                    price = 10.46;
+                }
+                else if (type == "Business")
+                {
+                    price = 16;
+                }
+                else if (type == "Regular")
+                {
+                    price = 22.50;
+                }
+            }
+
+            if (people >= 30 && type == "Students")
+            {
+                Discount = 0.85;
+            }
+            else if (people >= 100 && type == "Business")
+            {
+                people = people - 10;
+            }
+            else if (people >= 10 && people <= 20)
+            {
+                Discount = 0.95;
+            }
+
+            totalPrice = price * people * Discount;
+
+            Console.WriteLine($"Total price: {totalPrice:F2}");
+
+
+
+
+        }
+    }
+}
---
+++ Programming-Fundamentals-with-C#-Sep2025/01.BasicSyntax,ConditionalStatementsAndLoops-Exercise/05. Login/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string user = Console.ReadLine();
+            char[] pass = user.ToCharArray();
+            int tries = 0;
+            bool operationDone = false;
+
+            Array.Reverse(pass);
+
+            string reversePass = new string(pass);
+
+            for (int i = 1; i < 5; i++)
+            {
+                tries++;
+                string input = Console.ReadLine();
+                if (input == reversePass)
+                {
+                    Console.WriteLine($"User {user} logged in.");
+                    operationDone = true;
+                    break;
+                }
+                else
+                {
+                    if (tries == 4)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+
+
+            }
+
+            if (operationDone == false || tries == 4)
+            {
+                Console.WriteLine($"User {user} blocked!");
+            }
+        }
+    }
+}
